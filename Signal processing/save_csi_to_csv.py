0bdc8df9dad015b3c60be5e97baff8081ee8b747
GBhihi <dangngocgiabao01092005@gmail.com>
2026-03-12 20:10:36 +0700

    init project

---
+++ Signal processing/save_csi_to_csv.py
@@ -0,0 +1,34 @@
+import serial
+import csv
+
+PORT = "COM9"
+BAUD = 115200
+OUTPUT = "sit_raw5.csv"
+
+ser = serial.Serial(PORT, BAUD, timeout=1)
+
+with open(OUTPUT, "w", newline="") as f:
+    writer = csv.writer(f)
+
+    print("Recording CSI only... Ctrl+C to stop")
+
+    try:
+        while True:
+            line = ser.readline().decode(errors="ignore").strip()
+            if not line:
+                continue
+
+            parts = line.split(",")
+
+            # chỉ nhận dòng CSI (bắt đầu bằng RSSI)
+            try:
+                float(parts[0])
+            except:
+                continue
+
+            writer.writerow(parts)
+
+    except KeyboardInterrupt:
+        print("Stopped")
+
+ser.close()
