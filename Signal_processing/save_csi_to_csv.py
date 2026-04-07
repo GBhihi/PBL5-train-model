546ed60a898d1e75508e2c8ee79ccc5a69557ce6
GBhihi <dangngocgiabao01092005@gmail.com>
2026-04-07 22:30:24 +0700

    Fix model path in test.py to point to the correct checkpoint directory

--- Signal_processing/save_csi_to_csv.py
+++ Signal_processing/save_csi_to_csv.py
@@ -3,7 +3,7 @@ import csv
 
 PORT = "COM9"
 BAUD = 115200
-OUTPUT = "walk_raw1.csv"
+OUTPUT = "walk1.csv"
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
--- src/test.py
+++ src/test.py
@@ -14,7 +14,7 @@ from window import create_segments
 
 # --- 3. Đường dẫn file ---
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "../checkpoints/lstmcnn.pt")
+model_path = os.path.join(BASE_DIR, "../checkpoints(1)/lstmcnn.pt")
 csv_path = os.path.join(BASE_DIR, "../data/test/test_stand1.csv")
 
 # --- 4. Load checkpoint trước để lấy metadata ---
