21507a1e4f84d9c35501ca89e4e07c2f837efa5f
GBhihi <dangngocgiabao01092005@gmail.com>
2026-04-08 21:37:31 +0700

    Update model and CSV file paths in test.py for new checkpoint and data structure

--- src/test.py
+++ src/test.py
@@ -14,8 +14,8 @@ from window import create_segments
 
 # --- 3. Đường dẫn file ---
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "../checkpoints(1)/lstmcnn.pt")
-csv_path = os.path.join(BASE_DIR, "../data/test/test_stand1.csv")
+model_path = os.path.join(BASE_DIR, "../checkpoints(2)/lstmcnn.pt")
+csv_path = os.path.join(BASE_DIR, "../data/raw/sit.csv")
 
 # --- 4. Load checkpoint trước để lấy metadata ---
 ckpt = torch.load(model_path, map_location=torch.device("cpu"))
