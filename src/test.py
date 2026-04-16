db9fac413adaf00b213c0068e78fddda77e0d738
GBhihi <dangngocgiabao01092005@gmail.com>
2026-04-16 16:58:53 +0700

    Remove walk data handling from training and testing scripts

--- src/test.py
+++ src/test.py
@@ -14,8 +14,8 @@ from window import create_segments
 
 # --- 3. Đường dẫn file ---
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "../checkpoints(4)/lstmcnn.pt")
-csv_path = os.path.join(BASE_DIR, "../data/raw/stand.csv")
+model_path = os.path.join(BASE_DIR, "../checkpoints(5)/lstmcnn.pt")
+csv_path = os.path.join(BASE_DIR, "../data/raw/sit1.csv")
 
 # --- 4. Load checkpoint trước để lấy metadata ---
 ckpt = torch.load(model_path, map_location=torch.device("cpu"))
