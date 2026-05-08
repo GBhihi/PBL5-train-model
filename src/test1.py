ae36487744462fd693eebe1062f2bc9dbe2183aa
GBhihi <dangngocgiabao01092005@gmail.com>
2026-05-08 22:03:46 +0700

    Update model path and step parameter in training configuration and test scripts

--- src/test1.py
+++ src/test1.py
@@ -18,8 +18,8 @@ from window import create_segments
 
 # --- 3. Đường dẫn file ---
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "../checkpoints_amp/lstmcnn.pt")
-csv_path = os.path.join(BASE_DIR, "../data/test/test_sit.csv")
+model_path = os.path.join(BASE_DIR, "../checkpoints_amp1/lstmcnn.pt")
+csv_path = os.path.join(BASE_DIR, "../data/test/test_stand1.csv")
 # --- 4. Load checkpoint trước để lấy metadata ---
 ckpt = torch.load(model_path, map_location=torch.device("cpu"), weights_only=False)
 
