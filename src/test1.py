64148bab8310f9267e32dd30c5e6bf5eb4711583
GBhihi <dangngocgiabao01092005@gmail.com>
2026-05-14 14:20:48 +0700

    Update model path and parameters; add new CSV data files
    - Changed model path from "../checkpoints_amp9/lstmcnn.pt" to "../checkpoints_amp10/lstmcnn.pt" in src/test1.py.
    - Updated default window size from 2048 to 4096 and step from 1024 to 2048 in src/train.py.
    - Added multiple new CSV files to the data/raw directory for training and testing purposes.

--- src/test1.py
+++ src/test1.py
@@ -19,7 +19,7 @@ from src.window import create_segments
 
 # --- 3. Đường dẫn file ---
 BASE_DIR = os.path.dirname(__file__)
-model_path = os.path.join(BASE_DIR, "../checkpoints_amp9/lstmcnn.pt")
+model_path = os.path.join(BASE_DIR, "../checkpoints_amp10/lstmcnn.pt")
 csv_path = os.path.join(BASE_DIR, "../data/raw/no_test.csv")
 # --- 4. Load checkpoint trước để lấy metadata ---
 ckpt = torch.load(model_path, map_location=torch.device("cpu"), weights_only=False)
@@ -75,8 +75,8 @@ for k, v in sd.items():
 sd = new_sd
 
 # --- 5. Lấy đúng tham số train ---
-window_size = int(train_args.get("window_size", 2048))
-step = int(train_args.get("step", 1024))
+window_size = int(train_args.get("window_size", 4096))
+step = int(train_args.get("step", 2048))
 cutoff = float(train_args.get("cutoff", 0.1))
 use_hampel = bool(train_args.get("use_hampel", False))
 nperseg = train_args.get("nperseg", None)
